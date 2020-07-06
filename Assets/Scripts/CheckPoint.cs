62a092d1054b01f0f01cc9a42c82b80dd7311950
SergioGamero31 <64720452+SergioGamero31@users.noreply.github.com>
2020-07-05 22:32:26 -0500

    Initial commit

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+            collision.GetComponent<PlayerRespawn>().ReachedCheckPoint(transform.position.x, transform.position.y);
+        }
+    }
+}
