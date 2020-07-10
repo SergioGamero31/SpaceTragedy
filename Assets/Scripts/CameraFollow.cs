19ce32a4d7d195e7f2d91482ede5ff37e6069cb9
Gricel Ramos <64701184+Gricel18@users.noreply.github.com>
2020-07-09 19:01:08 -0500

    Update CameraFollow.cs

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -14,18 +14,4 @@ public class CameraFollow : MonoBehaviour
     {
         
     }
-
-
-    void FixedUpdate()
-    {
-        float posX = Mathf.SmoothDamp(transform.position.x,
-            follow.transform.position.x, ref velocity.x, smoothTime);
-        float posY = Mathf.SmoothDamp(transform.position.y,
-            follow.transform.position.y, ref velocity.y, smoothTime);
-
-        transform.position = new Vector3(
-            Mathf.Clamp(posX, minCamPos.x, maxCamPos.x),
-            Mathf.Clamp(posY, minCamPos.y, maxCamPos.y),
-            transform.position.z);
-    }
 }
