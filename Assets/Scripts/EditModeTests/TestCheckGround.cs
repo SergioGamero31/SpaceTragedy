90c41e737e616d381b62fa9a904e6cb8140a8050
Leandro <67281642+leandrolazo@users.noreply.github.com>
2020-07-12 12:41:21 -0500

    Update TestCheckGround.cs

--- Assets/Scripts/EditModeTests/TestCheckGround.cs
+++ Assets/Scripts/EditModeTests/TestCheckGround.cs
@@ -14,7 +14,6 @@ namespace Tests
         {
             // Use the Assert class to test conditions
         }
-
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
         // `yield return null;` to skip a frame.
         [UnityTest]
