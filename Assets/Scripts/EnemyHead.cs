4afd9152fa4036bb0d7d024cc4264afff1599b9b
SergioGamero31 <64720452+SergioGamero31@users.noreply.github.com>
2020-07-13 05:24:30 -0500

    Update Scene2, Scene3

--- Assets/Scripts/EnemyHead.cs
+++ Assets/Scripts/EnemyHead.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class EnemyHead : MonoBehaviour
 {
+    public GameObject camGameOver;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.CompareTag("Player"))
         {
             Debug.Log("Player Damaged");
             Destroy(collision.gameObject);
+            camGameOver.SetActive(true);
         }
     }
 
