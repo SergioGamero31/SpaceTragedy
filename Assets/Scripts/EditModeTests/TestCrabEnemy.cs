bcba10f2bed267ac9781b6dfe429f5b7dbee4be9
Gricel Ramos <gric2000@gmail.com>
2020-07-22 10:13:12 -0500

    TEST

---
+++ Assets/Scripts/EditModeTests/TestCrabEnemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestCrabEnemy
+    {
+        int crabmata;
+        [SetUp]
+        public void SetUpCrab()
+        {
+            Enemy crab;
+            crab = new Enemy();
+            crabmata = 0;
+        }
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TestCrabEnemySimplePasses()
+        {
+            // Use the Assert class to test conditions
+            crabmata = 1;
+            Assert.AreEqual(1, crabmata);
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator TestCrabEnemyWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/EditModeTests/TestDisparo.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestDisparo
+    {
+        BulletMove disparoDerecha;
+        BulletMove disparoIzquierda;
+        int dispararderecha;
+        int dispararizquierda;
+        [SetUp]
+        public void SetUpDisparo()
+        {
+            disparoDerecha = new BulletMove();
+            disparoIzquierda = new BulletMove();
+            dispararderecha = 0;
+            dispararizquierda = 0;
+        }
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TestDisparoIzquierda()
+        {
+            // Use the Assert class to test conditions
+            dispararizquierda = 1;
+            Assert.AreEqual(1, dispararizquierda);
+        }
+        [Test]
+        public void TestDisparoDerecha()
+        {
+            // Use the Assert class to test conditions
+            dispararderecha = 1;
+            Assert.AreEqual(1, dispararderecha);
+        }
+        [Test]
+        public void TestDisparoSimplePasses()
+        {
+            // Use the Assert class to test conditions
+
+        }
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator TestDisparoWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
